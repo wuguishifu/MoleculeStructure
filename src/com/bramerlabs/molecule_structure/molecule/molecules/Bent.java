ee082abddf1d0cd146126ab9adc5ae03e88f1739
Bo Bramer <bramer.bo@gmail.com>
2020-12-09 21:46:27 -0800

    Added Bent class. Added numAtoms to tetrahedral and bent class.

--- src/com/bramerlabs/molecule_structure/molecule/molecules/Bent.java
+++ src/com/bramerlabs/molecule_structure/molecule/molecules/Bent.java
@@ -4,7 +4,7 @@ import com.bramerlabs.molecule_structure.math.Vector3f;
 import com.bramerlabs.molecule_structure.molecule.Atom;
 import com.bramerlabs.molecule_structure.molecule.Molecule;
 
-public class Tetrahedral extends Molecule {
+public class Bent extends Molecule {
 
     // the position of the center of this molecule
     private Vector3f position;
@@ -13,14 +13,14 @@ public class Tetrahedral extends Molecule {
     private float bondLength;
 
     // vectors representing the direction of the bonds
-    private final static float[][] positions = new float[][]{{0, 0, 0}, {1, 1, -1}, {-1, 1, 1}, {-1, -1, -1}, {1, -1, 1}};
-    public final static int numAtoms = 5;
+    private final static float[][] positions = new float[][]{{0, 0, 0}, {1, 1, -1}, {-1, 1, 1}};
+    public final static int numAtoms = 3;
 
     /**
      * constructor for a specified position
      * @param position - a Vector3f representing the center point of the molecule (usually the central atom)
      */
-    public Tetrahedral(Vector3f position, float bondLength) {
+    public Bent(Vector3f position, float bondLength) {
         super();
         for (float[] a : positions) {
             this.addAtom(new Atom(new Vector3f(a).normalize(bondLength)));
